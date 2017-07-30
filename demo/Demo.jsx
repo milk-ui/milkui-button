08cd38c4e4f3940f492d922676aa83bd2dd277f0
caiyongmin <645589265@qq.com>
2017-07-30 17:49:56 +0800

    Refactor(Demo): rename the class name with the BEM specification.

--- demo/Demo.jsx
+++ demo/Demo.jsx
@@ -13,23 +13,23 @@ import './Demo.scss';
 class Demo extends Component {
   render() {
     return (
-      <div className="demo-panel">
-        <div className="demo-title">Button</div>
-        <div className="demo-description">
+      <div className="demo__panel">
+        <div className="demo__title">Button</div>
+        <div className="demo__description">
           This is a demo page, enjoy it! 😀 ✌️
         </div>
-        <div className="demo-item">
-          <div className="demo-button-group">
+        <div className="demo__item">
+          <div className="demo__button-group">
             <Button size="large" type="default">default</Button>
             <Button size="large" type="primary">primary</Button>
             <Button size="large" type="danger">danger</Button>
           </div>
-          <div className="demo-button-group">
+          <div className="demo__button-group">
             <Button size="normal" type="default">default</Button>
             <Button size="normal" type="primary">primary</Button>
             <Button size="normal" type="danger">danger</Button>
           </div>
-          <div className="demo-button-group">
+          <div className="demo__button-group">
             <Button size="small" type="default">default</Button>
             <Button size="small" type="primary">primary</Button>
             <Button size="small" type="danger">danger</Button>
